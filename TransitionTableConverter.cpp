7a835abf1e11772e836d9f531a88efee8bddae59
YousefZook <yousefzook@outlook.com>
2018-03-22 16:40:13 +0200

    Finish transformer - without testing.

--- TransitionTableConverter.cpp
+++ TransitionTableConverter.cpp
@@ -1,45 +1,48 @@
 #include "TransitionTableConverter.h"
 
 
-TransitionTableConverter::TransitionTableConverter()
-{
+TransitionTableConverter::TransitionTableConverter() {
     //ctor
 }
 
-TransitionTableConverter::~TransitionTableConverter()
-{
+TransitionTableConverter::~TransitionTableConverter() {
     //dtor
 }
 
-vector<vector<int>> NFAToTable(NFA* nfa)
-{
+// initializing static members
+vector<graph::State *> graph::allStates;
+Bimap<int, graph::State *> TransitionTableConverter::statesMap;
+Bimap<int, string> TransitionTableConverter::inputsMap;
+vector<vector<int>> TransitionTableConverter::transTable;
 
-    graph::State * dest, *src;
+vector<vector<int>> NFAToTable(NFA *nfa) {
+
+    graph::State *dest, *src;
     string trans;
     int destInt, srcInt, transInt;
 
-    for(vector<graph::State *>::iterator iter = graph::allStates.begin(); iter != graph::allStates.end(); ++iter)
-    {
+    for (vector<graph::State *>::iterator iter = graph::allStates.begin(); iter != graph::allStates.end(); ++iter) {
         src = *iter;
         // get src index in the table
-        srcInt = * TransitionTableConverter::statesMap.keysForValue(src).begin();
+        srcInt = *TransitionTableConverter::statesMap.keysForValue(src).begin();
 
         // for each state loop on all edges it has
-        graph::State * temp = * iter;
-        vector<pair<graph::State*, string>> nextStates = temp->nextStates;
-        for(vector<pair<graph::State*, string>>::iterator iter2 = nextStates.begin(); iter2 != nextStates.end(); ++iter2)
-        {
+        graph::State *temp = *iter;
+        vector<pair<graph::State *, string>> nextStates = temp->nextStates;
+        for (vector<pair<graph::State *, string>>::iterator iter2 = nextStates.begin();
+             iter2 != nextStates.end(); ++iter2) {
             dest = iter2->first;
             trans = iter2->second;
 
             // get table indecies
-            destInt = * TransitionTableConverter::statesMap.keysForValue(dest).begin();
-            transInt = * TransitionTableConverter::inputsMap.keysForValue(trans).begin();
+            destInt = *TransitionTableConverter::statesMap.keysForValue(dest).begin();
+            transInt = *TransitionTableConverter::inputsMap.keysForValue(trans).begin();
 
             vector<int>::iterator it;
-            vector<int> row = TransitionTableConverter::transTable.at(srcInt);
+            vector<int> row;
+            row = TransitionTableConverter::transTable[srcInt];
             it = row.begin();
-            row.insert(it+transInt, destInt);
+            row.insert(it + transInt, destInt);
 
         }
 
@@ -47,15 +50,12 @@ vector<vector<int>> NFAToTable(NFA* nfa)
 }
 
 
-NFA tableToNFA(vector<vector<int>> * table)
-{
+NFA tableToNFA(vector<vector<int>> *table) {
 
 }
 
-void initInputsMap(vector<string> * inputs)
-{
+void initInputsMap(vector<string> *inputs) {
 }
 
-void initStatesMap(vector<graph::State*> * states)
-{
+void initStatesMap(vector<graph::State *> *states) {
 }
