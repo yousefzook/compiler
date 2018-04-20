dc857a50c7d9c6292814157e6ce1e0027f4dde51
aboelhamd <aboelhamd.abotreka@gmail.com>
2018-04-21 01:16:10 +0200

    Q1 in sheet 4 is tested and it's right.

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,22 +10,20 @@ using namespace std;
 int main() {
 
 	// NonTerminals
-	NonTerminal* E = new NonTerminal("E");
-	NonTerminal* nE = new NonTerminal("E'");
-	NonTerminal* T = new NonTerminal("T");
-	NonTerminal* nT = new NonTerminal("T'");
-	NonTerminal* F = new NonTerminal("F");
-	NonTerminal* nF = new NonTerminal("F'");
-	NonTerminal* P = new NonTerminal("P");
+	NonTerminal* E = new NonTerminal("bexpr");
+	NonTerminal* nE = new NonTerminal("bexpr''");
+	NonTerminal* T = new NonTerminal("bterm");
+	NonTerminal* nT = new NonTerminal("bterm'");
+	NonTerminal* P = new NonTerminal("bfactor");
 
 	// Terminals
 	Terminal* leftBracket = new Terminal("(");
 	Terminal* rightBracket = new Terminal(")");
-	Terminal* a = new Terminal("a");
-	Terminal* b = new Terminal("b");
-	Terminal* plus = new Terminal("+");
-	Terminal* asterix = new Terminal("*");
-	Terminal* Em = new Terminal("Em");
+	Terminal* Not = new Terminal("not");
+	Terminal* Or = new Terminal("or");
+	Terminal* And = new Terminal("and");
+	Terminal* True = new Terminal("true");
+	Terminal* False = new Terminal("false");
 	Terminal* dollar = new Terminal("$");
 	Terminal* epsilon = new Terminal("L");
 
@@ -35,61 +33,46 @@ int main() {
 	nonTerminals.push_back(nE);
 	nonTerminals.push_back(T);
 	nonTerminals.push_back(nT);
-	nonTerminals.push_back(F);
-	nonTerminals.push_back(nF);
 	nonTerminals.push_back(P);
 
 	// preparing terminals
 	vector<Terminal*> terminals;
 	terminals.push_back(leftBracket);
 	terminals.push_back(rightBracket);
-	terminals.push_back(a);
-	terminals.push_back(b);
-	terminals.push_back(plus);
-	terminals.push_back(asterix);
-	terminals.push_back(Em);
+	terminals.push_back(Not);
+	terminals.push_back(Or);
+	terminals.push_back(And);
+	terminals.push_back(True);
+	terminals.push_back(False);
 	terminals.push_back(dollar);
 	terminals.push_back(epsilon);
 
 	// preparing first terminal vectors
 	vector<Terminal*> Efirst;
+	Efirst.push_back(Not);
 	Efirst.push_back(leftBracket);
-	Efirst.push_back(a);
-	Efirst.push_back(b);
-	Efirst.push_back(Em);
+	Efirst.push_back(True);
+	Efirst.push_back(False);
 
 	vector<Terminal*> nEfirst;
-	nEfirst.push_back(plus);
+	nEfirst.push_back(Or);
 	nEfirst.push_back(epsilon);
 
 	vector<Terminal*> Tfirst;
+	Tfirst.push_back(Not);
 	Tfirst.push_back(leftBracket);
-	Tfirst.push_back(a);
-	Tfirst.push_back(b);
-	Tfirst.push_back(Em);
+	Tfirst.push_back(True);
+	Tfirst.push_back(False);
 
 	vector<Terminal*> nTfirst;
-	nTfirst.push_back(leftBracket);
-	nTfirst.push_back(a);
-	nTfirst.push_back(b);
-	nTfirst.push_back(Em);
+	nTfirst.push_back(And);
 	nTfirst.push_back(epsilon);
 
-	vector<Terminal*> Ffirst;
-	Ffirst.push_back(leftBracket);
-	Ffirst.push_back(a);
-	Ffirst.push_back(b);
-	Ffirst.push_back(Em);
-
-	vector<Terminal*> nFfirst;
-	nFfirst.push_back(asterix);
-	nFfirst.push_back(epsilon);
-
 	vector<Terminal*> Pfirst;
+	Pfirst.push_back(Not);
 	Pfirst.push_back(leftBracket);
-	Pfirst.push_back(a);
-	Pfirst.push_back(b);
-	Pfirst.push_back(Em);
+	Pfirst.push_back(True);
+	Pfirst.push_back(False);
 
 	// preparing first
 	map<NonTerminal*, vector<Terminal*> > first;
@@ -97,56 +80,32 @@ int main() {
 	first[nE] = nEfirst;
 	first[T] = Tfirst;
 	first[nT] = nTfirst;
-	first[F] = Ffirst;
-	first[nF] = nFfirst;
 	first[P] = Pfirst;
 
 	// preparing follow terminal vectors
 	vector<Terminal*> Efollow;
-	Efollow.push_back(rightBracket);
 	Efollow.push_back(dollar);
+	Efollow.push_back(rightBracket);
 
 	vector<Terminal*> nEfollow;
-	nEfollow.push_back(rightBracket);
 	nEfollow.push_back(dollar);
+	nEfollow.push_back(rightBracket);
 
 	vector<Terminal*> Tfollow;
-	Tfollow.push_back(plus);
-	Tfollow.push_back(rightBracket);
+	Tfollow.push_back(Or);
 	Tfollow.push_back(dollar);
+	Tfollow.push_back(rightBracket);
 
 	vector<Terminal*> nTfollow;
-	nTfollow.push_back(plus);
-	nTfollow.push_back(rightBracket);
+	nTfollow.push_back(Or);
 	nTfollow.push_back(dollar);
-
-	vector<Terminal*> Ffollow;
-	Ffollow.push_back(leftBracket);
-	Ffollow.push_back(a);
-	Ffollow.push_back(b);
-	Ffollow.push_back(Em);
-	Ffollow.push_back(plus);
-	Ffollow.push_back(rightBracket);
-	Ffollow.push_back(dollar);
-
-	vector<Terminal*> nFfollow;
-	nFfollow.push_back(leftBracket);
-	nFfollow.push_back(a);
-	nFfollow.push_back(b);
-	nFfollow.push_back(Em);
-	nFfollow.push_back(plus);
-	nFfollow.push_back(rightBracket);
-	nFfollow.push_back(dollar);
+	nTfollow.push_back(rightBracket);
 
 	vector<Terminal*> Pfollow;
-	Pfollow.push_back(asterix);
-	Pfollow.push_back(leftBracket);
-	Pfollow.push_back(a);
-	Pfollow.push_back(b);
-	Pfollow.push_back(Em);
-	Pfollow.push_back(plus);
-	Pfollow.push_back(rightBracket);
+	Pfollow.push_back(And);
+	Pfollow.push_back(Or);
 	Pfollow.push_back(dollar);
+	Pfollow.push_back(rightBracket);
 
 	// preparing follow
 	map<NonTerminal*, vector<Terminal*> > follow;
@@ -154,8 +113,6 @@ int main() {
 	follow[nE] = nEfollow;
 	follow[T] = Tfollow;
 	follow[nT] = nTfollow;
-	follow[F] = Ffollow;
-	follow[nF] = nFfollow;
 	follow[P] = Pfollow;
 
 	// preparing productions and symbols vectors
@@ -169,8 +126,9 @@ int main() {
 	vector<vector<Symbol*> > nEproduction;
 
 	vector<Symbol*> nE1;
-	nE1.push_back(plus);
-	nE1.push_back(E);
+	nE1.push_back(Or);
+	nE1.push_back(T);
+	nE1.push_back(nE);
 	nEproduction.push_back(nE1);
 
 	vector<Symbol*> nE2;
@@ -180,56 +138,41 @@ int main() {
 	vector<vector<Symbol*> > Tproduction;
 
 	vector<Symbol*> T1;
-	T1.push_back(F);
+	T1.push_back(P);
 	T1.push_back(nT);
 	Tproduction.push_back(T1);
 
 	vector<vector<Symbol*> > nTproduction;
 
 	vector<Symbol*> nT1;
-	nT1.push_back(T);
+	nT1.push_back(And);
+	nT1.push_back(P);
+	nT1.push_back(nT);
 	nTproduction.push_back(nT1);
 
 	vector<Symbol*> nT2;
 	nT2.push_back(epsilon);
 	nTproduction.push_back(nT2);
 
-	vector<vector<Symbol*> > Fproduction;
-
-	vector<Symbol*> F1;
-	F1.push_back(P);
-	F1.push_back(nF);
-	Fproduction.push_back(F1);
-
-	vector<vector<Symbol*> > nFproduction;
-
-	vector<Symbol*> nF1;
-	nF1.push_back(asterix);
-	nF1.push_back(F);
-	nFproduction.push_back(nF1);
-
-	vector<Symbol*> nF2;
-	nF2.push_back(epsilon);
-	nFproduction.push_back(nF2);
-
 	vector<vector<Symbol*> > Pproduction;
 
 	vector<Symbol*> P1;
-	P1.push_back(leftBracket);
-	P1.push_back(E);
-	P1.push_back(rightBracket);
+	P1.push_back(Not);
+	P1.push_back(P);
 	Pproduction.push_back(P1);
 
 	vector<Symbol*> P2;
-	P2.push_back(a);
+	P2.push_back(leftBracket);
+	P2.push_back(E);
+	P2.push_back(rightBracket);
 	Pproduction.push_back(P2);
 
 	vector<Symbol*> P3;
-	P3.push_back(b);
+	P3.push_back(True);
 	Pproduction.push_back(P3);
 
 	vector<Symbol*> P4;
-	P4.push_back(Em);
+	P4.push_back(False);
 	Pproduction.push_back(P4);
 
 	// preparing productions
@@ -238,8 +181,6 @@ int main() {
 	productions[nE] = nEproduction;
 	productions[T] = Tproduction;
 	productions[nT] = nTproduction;
-	productions[F] = Fproduction;
-	productions[nF] = nFproduction;
 	productions[P] = Pproduction;
 
 	map<NonTerminal*, map<Terminal*, int> > parsingTable =
